0c55a8889f23f58f522810a9ff887b3c0deded2e
Niklas <niklas.beuster@tu-ilmenau.de>
2017-09-12 19:17:28 +0200

    gui icons are now included when installing

--- setup.py
+++ setup.py
@@ -4,27 +4,22 @@
 from __future__ import nested_scopes, generators, with_statement, unicode_literals, absolute_import, division, print_function
 
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def read(fname):
 	'''open the long description of the project'''
 	return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-	name='satcomdevctrl',
-	version='0.1',
+	name='flow',
+	version='0.0.1',
 	author='Niklas Beuster',
-	author_email='beustens@iis.fraunhofer.de',
-	description=('Device classes to communicate with several devices located at the FORTE lab in Ilmenau'),
-	license='Fraunhofer IIS/DVT',
-	keywords='device measurements satcom',
-	url='https://git01.iis.fhg.de/dvt-forte/SatcomDevCtrl',
-	packages=['satcomdevctrl', 'satcomdevctrl.interfaces'],
+	author_email='niklas.beuster@tu-ilmenau.de',
+	description=('Flow based programming with an optional GUI'),
+	keywords='flowbased programming gui',
+	url='https://makalu3.rz.tu-ilmenau.de/nibe8075/Flow',
+	packages=find_packages(),
+	include_package_data=True, 
 	long_description=read('README.md'),
-	install_requires=['pyvisa-py', 'pyserial>=3.0'],
-	classifiers=[
-		'Development Status :: 1 - Alpha',
-		'Topic :: Utilities',
-		'License :: Fraunhofer :: proprietary',
-	],
+	install_requires=[],
 )
